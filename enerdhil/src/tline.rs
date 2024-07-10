e951fceac647a1e6bc91943d0a3e5f97ba69da8b
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2024-07-10 09:42:58 -0400

    feat: complex (dispersive/lossy) transmission lines

--- enerdhil/src/tline.rs
+++ enerdhil/src/tline.rs
@@ -1,10 +1,10 @@
-use std::f64::consts::PI;
+use std::f64::consts::{E, PI};
 use thiserror::Error;
 
 use num::complex::{Complex64, ComplexFloat};
 
-use crate::util::cohn_k;
 use crate::sim::*;
+use crate::util::{cohn_k, disperse_f, hammerstad_z, ms_alpha_c, ms_alpha_d};
 
 pub struct TLineProps {
     zed: f64,
@@ -14,13 +14,18 @@ pub struct TLineProps {
     wavelengths: f64,
     /// In mm
     p_len: f64,
+    p_width: f64,
     super_line: bool,
     alpha_d: f64,
     alpha_c: f64,
+    /// For super lines, f=0 (???)
+    zed_e0: f64,
+    zed_s_e0: f64,
+    e_eff_e0: f64,
 }
 
 impl TLineProps {
-    pub fn from_mm(zed: f64, len_mm: f64, sim: &SimProps) -> Result<Self, TLineError> {
+    pub fn from_mm(zed: f64, len_mm: f64, adv: bool, sim: &SimProps) -> Result<Self, TLineError> {
         let width = width_tline(zed, sim)?;
         let ere = match sim.mode {
             SimType::Microstrip => {
@@ -31,15 +36,192 @@ impl TLineProps {
         };
         let fs_wlen = sim.lambda_fd_mm();
         let wavelengths = len_mm * ere.sqrt() / fs_wlen;
-        Ok(TLineProps {
+        let mut line = TLineProps {
             zed,
             e_len: 2.0 * PI * wavelengths,
             wavelengths,
             p_len: len_mm,
+            p_width: width,
             super_line: false,
             alpha_d: 0.0,
             alpha_c: 0.0,
-        })
+            // unused by non-super routines.
+            zed_e0: 0.0,
+            zed_s_e0: 0.0,
+            e_eff_e0: 0.0,
+        };
+        if adv {
+            match sim.mode {
+                SimType::Stripline => line.super_stripline(sim),
+                SimType::Microstrip => line.super_microstrip(sim),
+            }
+        }
+        Ok(line)
+    }
+
+    /** Re-analyze microstrip parameters using the W/h
+    derived from static calculation.
+    Uses Hammerstad and Jensen (1980 MTT-S) models
+    for Z0 and e_eff (see Itoh's IEEE book).
+
+    Affects:  tcompt^.zed,  (new value from W/h)
+        tcompt^.e_eff_e0   (e_eff value at f=0)
+        tcompt^.alpha_c,  (value at fd)
+        tcompt^.alpha_d   (value at fd)
+        tcompt^.zed_e0,   (new f=0 value from W/h)
+        tcompt^.wavelength  (new value from lngth0)
+    */
+    fn super_microstrip(&mut self, sim: &SimProps) {
+        fn a(u: f64) -> f64 {
+            1.0 + ((u.powi(4) + (u / 52.).powi(2)) / (u.powi(4) + 0.432)).ln() / 49.
+                + (u / 18.1).powi(3).ln_1p() / 18.7
+        }
+        fn e_e(u: f64, b: f64, er: f64) -> f64 {
+            (er + 1.0) / 2. + ((er - 1.0) / 2.) * (1.0 + 10. / u).powf(-a(u) * b)
+        }
+        let u_in = self.p_width / sim.height;
+        let b = 0.564 * ((sim.epsilon_r - 0.9) / (sim.epsilon_r + 3.0)).powf(0.053);
+        let t_n = sim.metal_thickness / sim.height;
+        let (mut delta_ul, mut delta_ur) = (0.0, 0.0);
+        if t_n > 0.0 {
+            delta_ul = (t_n / PI)
+                * ((4. * E)
+                    / (t_n
+                        * ((6.517 * u_in).sqrt().cosh() / (6.517 * u_in).sqrt().sinh()).powi(2)))
+                .ln_1p();
+            delta_ur = 0.5 * (1.0 + 1.0 / (sim.epsilon_r - 1.0).sqrt().cosh()) * delta_ul;
+        }
+        let ul = u_in + delta_ul;
+        let ur = u_in + delta_ur;
+        let z0 = hammerstad_z(ur) / e_e(ur, b, sim.epsilon_r).sqrt();
+        let e_eff = e_e(ur, b, sim.epsilon_r) * (hammerstad_z(ul) / hammerstad_z(ur)).powi(2);
+
+        /*For ms_dispersion, compute value for zed_S_e0 from
+        equivalent zero thickness stripline with b=2h.
+        Double the value obtained from the formulas with b=2h.
+        Use super_stripline formulas with t=0, b=2*h */
+        let b_t_w = 2. / u_in;
+        let a_fac = ((4. * b_t_w / PI)
+            * ((8. * b_t_w / PI) + ((8. * b_t_w / PI).powi(2) + 6.27).sqrt()))
+        .ln_1p();
+        let z0_t = 60. * a_fac / sim.epsilon_r.sqrt();
+
+        self.alpha_c = ms_alpha_c(self.p_width, z0, e_eff, sim);
+        self.alpha_d = ms_alpha_d(e_eff, sim);
+        self.zed = z0;
+        self.zed_e0 = z0;
+        self.zed_s_e0 = z0_t;
+        self.e_eff_e0 = e_eff;
+        self.wavelengths = self.p_len * e_eff.sqrt() / sim.lambda_fd_mm();
+        self.e_len = 2. * PI * self.wavelengths;
+        self.super_line = true;
+    }
+
+    /**	Re-analyze stripline parameters using W/h
+    from static calculation, adding in the effects
+    of finite strip thickness. Re-calculates Z0.
+    Computes stripline alpha due to conductor loss.
+    See Gupta, Garg, and Chadha pp 59-60.
+
+    Affects:  tcompt^.zed,
+          tcompt^.alpha_c,
+          tcompt^.alpha_d */
+    fn super_stripline(&mut self, sim: &SimProps) {
+        if sim.metal_thickness > 0.0 {
+            let w = self.p_width;
+            let b = sim.height;
+            // Equation (3.41b)
+            let x = sim.metal_thickness / b;
+            // Calculation of Z_0 with finite thickness metal
+            // (3.41b)
+            let m = 2. / (1.0 + 2. * x / (3. * (1.0 - x)));
+            // (3.41a)
+            let delta_w_b_t = x
+                * (1.0
+                    - 0.5
+                        * ((x / (2.0 - x)).powi(2) + (0.0796 * x / ((w / b) + 1.1 * x)).powf(m))
+                            .ln())
+                / (PI * (1.0 - x));
+            // (3.40)
+            let w_over_b_t = (w / (b - sim.metal_thickness)) + delta_w_b_t;
+            let w_prime = (b - sim.metal_thickness) * w_over_b_t;
+            let b_t_w = w_over_b_t.recip();
+            // a_fac is different from the pascal by a factor of pi.
+            // This is a chunk of (3.39), because it's reused in (3.48).
+            let a_fac = ((4. * b_t_w / PI)
+                * ((8. * b_t_w / PI) + ((8. * b_t_w / PI).powi(2) + 6.27).sqrt()))
+            .ln_1p();
+            // (3.39)
+            let z0_t = 30. * a_fac / sim.epsilon_r.sqrt();
+            // Calculation of alpha_c
+            // (3.49)
+            let q_fac = (1.0 + 6.27 * (PI * w_over_b_t / 8.).powi(2)).sqrt();
+            // Note: this is missing a epsilon_r.sqrt() factor that cancels with a term in alpha_c (3.47).
+            // (3.48)
+            // TODO confirm the 0.303/q_fac.
+            let partial_z_w = 30.0
+                * (3.135 / q_fac
+                    - (1. + q_fac - 0.303 / q_fac) * (8.0 / (PI * w_over_b_t)).powi(2))
+                / (a_fac.exp() * w_prime);
+            // (3.47)
+            let log_term = 1. + 2. * w_over_b_t - (3. * x / (2. - x) + (x / (2. - x)).ln()) / PI;
+            // TODO check that this really should be a minus.
+            self.alpha_c = -sim.rs_at_fd() * partial_z_w * log_term / (120. * PI * z0_t);
+        }
+        self.super_line = true;
+        self.alpha_d = PI * sim.epsilon_r.sqrt() * sim.loss_tangent / sim.lambda_fd_mm();
+    }
+
+    /** Calculate effective dielectric constant and Z
+    as a function of frequency for microstrip.
+
+    Input values from super_microstrip are:
+        tcompt^.zed_e0   := advanced model for zed at f=0
+        tcompt^.e_eff_e0 := new ere at f=0
+        tcompt^.lngth0   := lngth in mm
+
+    Requires W/h from static calculation, epsilon(0)
+    from supermicrostrip calculation, and
+    frequency normalized to board thickness.
+
+    Uses M. Kirschning and R.H. Jansen ere model
+    from Electronics Letters, vol. 18, pp. 272-273,
+    March, 1982. See also H. Atwater and York
+    and Compton papers.
+
+    Uses Hammerstad and Jensen formula for frequency
+    dependence of Z0. */
+    fn zed_elen(&self, freq: f64, sim: &SimProps) -> (f64, f64) {
+        if matches!(sim.mode, SimType::Stripline) || !self.super_line {
+            return (self.zed, self.e_len);
+        }
+        // F4 = f*h in units of GHz-cm (freq normalized to board thickness)
+        let f4 = sim.height * freq / 1e10;
+        if f4 > 25.0 {
+            // Unstable, substitute asymptotic values
+            (
+                self.zed_s_e0,
+                2. * PI * self.p_len * sim.epsilon_r.sqrt() / sim.lambda_fd_mm(),
+            )
+        } else if f4 > 0.0 {
+            let u_in = self.p_width / sim.height;
+            let p1 = 0.27488 + (0.6315 + 0.525 / (1.0 + 0.157 * f4).powi(20)) * u_in
+                - 0.065683 * (-8.7513 * u_in).exp();
+            let p2 = 0.33622 * (1.0 - (-0.03442 * sim.epsilon_r).exp());
+            let p3 = 0.0363 * (-4.6 * u_in).exp() * -(-1.0 * (f4 / 3.87).powf(4.97)).exp_m1();
+            let p4 = 1.0 + 2.751 * (-1.0 * (sim.epsilon_r / 15.916).powi(8)).exp_m1();
+            let p = p1 * p2 * ((0.1844 + p3 * p4) * 10. * f4).powf(1.5763);
+            let ere_f = disperse_f(sim.epsilon_r, self.e_eff_e0, p);
+            (
+                disperse_f(self.zed_s_e0, self.zed_e0, p),
+                2. * PI * self.p_len * ere_f.sqrt() / sim.lambda_fd_mm(),
+            )
+        } else {
+            (
+                self.zed_e0,
+                2. * PI * self.p_len * self.e_eff_e0.sqrt() / sim.lambda_fd_mm(),
+            )
+        }
     }
 }
 
@@ -48,7 +230,6 @@ impl TwoPort for TLineProps {
         tline_sim(freq, self, sim)
     }
 }
-
 
 /// TODO check if this is really necessary.
 fn recip_finite(x: Complex64) -> Complex64 {
@@ -62,17 +243,15 @@ fn recip_finite(x: Complex64) -> Complex64 {
 fn tline_sim(freq: f64, line: &TLineProps, sim: &SimProps) -> [Complex64; 4] {
     // Normalized frequency
     let gamma = freq / sim.design_freq;
-    if line.super_line {
-        todo!("Dispersive tlines");
-    }
-    let beta_l = line.e_len * gamma;
+    let (zed, e_len) = line.zed_elen(freq, sim);
+    let beta_l = e_len * gamma;
     let alpha_tl =
         (line.alpha_d * gamma + rough_alpha(line.alpha_c, freq, sim) * gamma.sqrt()) * line.p_len;
     let exp = Complex64::new(alpha_tl, beta_l);
     // I'm 90% sure this is correct. TODO check by hand again.
     let sh = exp.sinh();
     let ch = exp.cosh();
-    let zd = line.zed / sim.z0;
+    let zd = zed / sim.z0;
     // s11, s12, s21, s22
     let mut s_params = [Complex64::ZERO; 4];
     let rds = recip_finite(2.0 * zd * ch + (1.0 + zd.powi(2)) * sh);
@@ -101,6 +280,7 @@ fn rough_alpha(alpha: f64, freq: f64, sim: &SimProps) -> f64 {
 ///
 /// Microstrip models are from Owens:
 ///     Radio and Elect Eng, 46, pp 360-364, 1976.
+///     (https://doi.org/10.1049/ree.1976.0058)
 /// Stripline models are from  Cohn:
 ///     MTT-3 pp19-126, March 1955.
 /// See also Gupta, Garg, Chadha:
@@ -157,11 +337,11 @@ mod tests {
     use serde_json;
 
     #[test]
-    fn write_json_data() {
+    fn write_json_25ohm() {
         let min = 0.0;
         let max = 10.0E9;
         let n = 201;
-        let step = (max - min) / (n-1) as f64;
+        let step = (max - min) / (n - 1) as f64;
         let mut sparams: Vec<[Complex64; 4]> = Vec::new();
         let sim = SimProps {
             mode: SimType::Microstrip,
@@ -173,12 +353,43 @@ mod tests {
             eps0: 8.8541878128e-12,
             epsilon_r: 10.2,
             height: 1.27,
+            loss_tangent: 0.02,
+            metal_thickness: 0.035,
         };
-        let line = TLineProps::from_mm(25.0, 12.0, &sim).expect("Hard-coded tline should work.");
+        let line =
+            TLineProps::from_mm(25.0, 12.0, false, &sim).expect("Hard-coded tline should work.");
         for i in 0..n {
             sparams.push(line.simulate(i as f64 * step, &sim));
         }
         let file = File::create("test/data/25ohm.json").expect("Failed to open test data file.");
+        serde_json::to_writer_pretty(file, &sparams).expect("Unable to serialize s-parameters.");
+    }
+    #[test]
+    fn write_json_s25ohm() {
+        let min = 0.0;
+        let max = 10.0E9;
+        let n = 201;
+        let step = (max - min) / (n - 1) as f64;
+        let mut sparams: Vec<[Complex64; 4]> = Vec::new();
+        let sim = SimProps {
+            mode: SimType::Microstrip,
+            design_freq: 3e9,
+            surface_roughness: 2.0,
+            conductivity: 5.8e7,
+            z0: 50.0,
+            mu0: 1.25663706212e-6,
+            eps0: 8.8541878128e-12,
+            epsilon_r: 10.2,
+            height: 1.27,
+            loss_tangent: 0.02,
+            metal_thickness: 0.035,
+        };
+        let line =
+            TLineProps::from_mm(25.0, 12.0, true, &sim).expect("Hard-coded tline should work.");
+        for i in 0..n {
+            sparams.push(line.simulate(i as f64 * step, &sim));
+        }
+        let file = File::create("test/data/s25ohm.json").expect("Failed to open test data file.");
         serde_json::to_writer_pretty(file, &sparams).expect("Unable to serialize s-parameters.");
     }
 }
--- enerdhil/src/util.rs
+++ enerdhil/src/util.rs
@@ -1,5 +1,7 @@
 use std::f64::consts::PI;
 
+use crate::sim::SimProps;
+
 /// Function used to calculate Cohn's "k" factor for stripline
 /// width formulas.  See equation 3.6, page 13 of the Puff Manual.
 ///
@@ -12,4 +14,58 @@ pub(crate) fn cohn_k(x: f64) -> f64 {
         let expx = (PI / x).exp();
         ((expx - 2.0) / (expx + 2.0)).powi(2)
     }
+}
+///  Calculates Z_o of microstrip using
+/// Hammerstad and Jensen model.
+///
+/// Used by super_microstrip and super_cl_microstrip
+/// This value must be divided by sqrt(ere) before use.
+pub(crate) fn hammerstad_z(u: f64) -> f64 {
+    let f = 6.0 + (2. * PI - 6.0) * (-1.0 * (0.7528 * (30.666 / u).ln()).exp()).exp();
+    60. * (f / u + (1.0 + (2. / u).powi(2)).sqrt()).ln()
+}
+
+/// Used to compute dielectric losses for
+/// microstrip tlines and clines.
+pub(crate) fn ms_alpha_d(ere_in: f64, sim: &SimProps) -> f64 {
+    if sim.epsilon_r != 1.0 {
+        PI * sim.epsilon_r * (ere_in - 1.0) / (ere_in.sqrt() * (sim.epsilon_r - 1.0))
+            * sim.loss_tangent
+            / sim.lambda_fd_mm()
+    } else {
+        PI * sim.epsilon_r.sqrt() * sim.loss_tangent / sim.lambda_fd_mm()
+    }
+}
+/// Computes microstrip alpha due to conductor loss.
+/// Uses effective width calculations, returns Np/mm.
+/// See Gupta, Garg, and Bahl pp 91-92.
+///
+/// W_in is uncorrected (actual) width, Z_0_in is
+/// previously calculated Z.
+pub(crate) fn ms_alpha_c(w_in: f64, z0_in: f64, er_e: f64, sim: &SimProps) -> f64 {
+    if sim.metal_thickness > 0.0 {
+        // In the pascal source, there was a complicated if/else.
+        // I think it simplifies down to this.
+        // Ugh, I can't use cmp::min here. floating-point moment.
+        let b_fac = sim.height.min(2. * PI * w_in);
+        let we_over_h = (PI * w_in
+            + 1.25 * sim.metal_thickness * (1.0 + (2. * b_fac / sim.metal_thickness).ln()))
+            / (PI * sim.height);
+        let a_fac = 1.0 + (1.0 + 1.25 * (2. * b_fac / sim.metal_thickness).ln() / PI) / we_over_h;
+        // Previously a condition on w_over_h, changed for readability.
+        let w_h_ratio = if w_in < sim.height {
+            (32.0 - we_over_h.powi(2)) / (32.0 + we_over_h.powi(2)) / (2. * PI * z0_in)
+        } else {
+            // TODO: replace with exact versions of these constants?
+            (0.667 * we_over_h / (we_over_h + 1.444) + we_over_h) * z0_in * er_e
+                / (120. * PI).powi(2)
+        };
+        a_fac * w_h_ratio * sim.rs_at_fd() / sim.height
+    } else {
+        0.0
+    }
+}
+
+pub(crate) fn disperse_f(er1: f64, er2: f64, f_eo: f64) -> f64 {
+    er1 - (er1 - er2) / (1.0 + f_eo)
 }

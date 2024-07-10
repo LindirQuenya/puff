e6d621ceff78414790a3ffbbc703916bb7cf8ee4
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2024-07-10 09:42:33 -0400

    refactor: restructure light calculations to include a function for c

--- enerdhil/src/lib.rs
+++ enerdhil/src/lib.rs
@@ -1,5 +1,5 @@
-pub mod tline;
 pub mod sim;
+pub mod tline;
 pub(crate) mod util;
 
 pub fn add(left: usize, right: usize) -> usize {
--- enerdhil/src/sim.rs
+++ enerdhil/src/sim.rs
@@ -1,7 +1,13 @@
+use std::f64::consts::PI;
+
 use num::complex::Complex64;
 
 pub trait TwoPort {
     fn simulate(&self, freq: f64, sim: &SimProps) -> [Complex64; 4];
+}
+
+pub trait Parseable {
+    fn parse(spec: &String) -> Self;
 }
 
 pub enum SimType {
@@ -26,12 +32,23 @@ pub struct SimProps {
     pub epsilon_r: f64,
     /// Dielectric height, mm
     pub height: f64,
+    /// e.g. 0.02 for FR4.
+    pub loss_tangent: f64,
+    /// millimeters
+    pub metal_thickness: f64,
 }
 
 impl SimProps {
-    /// Gives the free-space wavelength in mm.
+    /// The free-space wavelength in mm.
     pub fn lambda_fd_mm(&self) -> f64 {
-        let a = ((self.mu0 * self.eps0).sqrt() * self.design_freq).recip() * 1000.0;
-        a
+        self.c_mm() / self.design_freq
+    }
+    /// The speed of light in mm/s.
+    pub fn c_mm(&self) -> f64 {
+        1000. / (self.mu0 * self.eps0).sqrt()
+    }
+    /// The sheet resistance of the metal at the design frequency.
+    pub fn rs_at_fd(&self) -> f64 {
+        (PI * self.design_freq * self.mu0 / self.conductivity).sqrt()
     }
 }
